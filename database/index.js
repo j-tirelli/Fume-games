b8305215b5b74762dd8b6c57d89e6076c31b366d
j-tirelli <john.tirelli@gmail.com>
2020-09-24 16:00:31 -0700

    Initial sequelize connection

---
+++ database/index.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('reviews', 'reviewsApp', 'reviewsApp1234', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+};
