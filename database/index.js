2083421dad1cee1c5c7e330a1a8a59aee04742d9
j-tirelli <john.tirelli@gmail.com>
2020-09-26 01:39:44 -0700

    Created db logic to get all reviews for one game, logic to change review metadata and scripts to initialize and seed the database

--- database/index.js
+++ database/index.js
@@ -1,5 +1,6 @@
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize');
 const dotenv = require('dotenv');
+const models = require('./models');
 dotenv.config();
 
 const sequelize = new Sequelize('reviews', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
@@ -7,9 +8,27 @@ const sequelize = new Sequelize('reviews', process.env.DB_USERNAME, process.env.
   dialect: 'mysql'
 });
 
-try {
-  sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
+var getReviews = async (game_id) => {
+  const reviews = await models.Review.findAll({ where: { game_id } });
+  return reviews;
 };
+
+var updateReview = async (id, dataToChange) => {
+  const review = await models.Review.findOne({ where: { id } });
+  if (dataToChange.key === 'awards') {
+    let parsedAwards = JSON.parse(review.awards);
+    parsedAwards[dataToChange.val]++;
+    review.awards = JSON.stringify(parsedAwards);
+  } else if (dataToChange.key === 'funny') {
+    review.funny_count++
+  } else if (dataToChange.key === 'helpful') {
+    review.helpful_count++
+  } else if (dataToChange.key === 'unhelpful') {
+    review.helpful_count--
+  }
+  review.save();
+  return review;
+};
+
+module.exports.getReviews = getReviews;
+module.exports.updateReview = updateReview;
