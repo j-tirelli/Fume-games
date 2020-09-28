b78bf365c4dcc2e6e02cd4954e60d69d44f2c8ed
j-tirelli <john.tirelli@gmail.com>
2020-09-28 09:44:50 -0700

    Created unmigrate scripts and clearDB script to completely clear out DB of all related entries, tables and databases. First attempt at user_game join table seed script, currently not functioning.

--- database/seeders/20200925062745-User.js
+++ database/seeders/20200925062745-User.js
@@ -10,8 +10,8 @@ module.exports = {
       var fakeDate = faker.date.between('2009-01-01', '2020-09-05');
       var fakeName = faker.internet.userName();
       var fakePic = faker.internet.avatar();
-      var fakeOwned = Math.floor(Math.random() * 200);
-      var fakeReviewed = Math.floor(Math.random() * 100);
+      var fakeOwned = Math.floor(Math.random() * 100);
+      var fakeReviewed = Math.floor(Math.random() * 50);
       if (!noDupe[fakeName]) {
         fakeUsers.push({Username: fakeName, avatar: fakePic, games_owned_count: fakeOwned, reviews_count: fakeReviewed, createdAt: fakeDate, updatedAt: fakeDate});
         noDupe[fakeName] = true;
