94260f333b36e197003c66a226a0041ab32810b7
j-tirelli <john.tirelli@gmail.com>
2020-09-30 11:03:04 -0700

    Minor changes to index.html, added es6 transpiler preset, reconfigured webpack.config.js properly

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,15 +1,27 @@
-module: {
-  rules: [
-    {
-      mode: 'development',
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
         }
       }
-    }
-  ]
-}
+    ]
+  }
+};
